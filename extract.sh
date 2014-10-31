338ce9d884d44bb9cef52bd488e3abae0b518b73
N R Callahan <nicholas.r.callahan@gmail.com>
2014-10-31 03:52:46 -0700

    Extract.sh will take the given file and extract the northing/eastings and the zone from an OpenTopo metadata file.

---
+++ extract.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+if [ ! $# -eq 1 ]; then
+    echo "Usage: extract.sh [FILE]"
+    exit
+fi;
+
+if [ ! -f $1 ]; then
+    echo "File not found!"
+    exit
+fi;
+
+i=0
+xmin=""
+xmax=""
+ymin=""
+ymax=""
+zone=""
+while read line
+do
+    if [ $i -eq 26 ]; then
+        xmin=$line
+    elif [ $i -eq 27 ]; then
+        xmax=$line
+    elif [ $i -eq 28 ]; then
+        ymin=$line
+    elif [ $i -eq 29 ]; then
+        ymax=$line
+    elif [ $i -eq 9 ]; then
+        zone=$line
+    fi;
+((i++))
+done < $1
+
+
+echo $zone
+xmin=${xmin##*: }
+xmax=${xmax##*: }
+ymin=${ymin##*: }
+ymax=${ymax##*: }
+zone=${zone##*UTM }
+zone=${zone%% *}
+zone=${zone:1}
+hemi=${zone:2}
+zone=${zone%?}
+echo "XMIN: $xmin"
+echo "XMAX: $xmax"
+echo "YMIN: $ymin"
+echo "YMAX: $ymax"
+echo $zone 
+echo $hemi

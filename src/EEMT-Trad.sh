66eb48aca471622d3e99517b5e1d19278f7251e2
Austin Paine <austinpaine@email.arizona.edu>
2014-12-16 21:02:55 -0700

    changed to correct tmin and tmax vars in EEMT scripts

--- src/EEMT-Trad.sh
+++ src/EEMT-Trad.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+month = $1
+
 
 # inputs DEM 10m from OpenTopo, 1km DEM from of DAYMET data
 
 # break up the two inputs into monthly inputs to do these calculations
 
 #Locally correct the temperature
-r.mapcalc tmin_loc=tmin-0.00649*(dem_10m-dem_1km)
-r.mapcalc tmax_loc=tmax-0.00649*(dem_10m-dem_1km)
+r.mapcalc tmin_loc=tmin_$month-0.00649*(dem_10m-dem_1km)
+r.mapcalc tmax_loc=tmax_$month-0.00649*(dem_10m-dem_1km)
 
 
 r.mapcalc f_tmin_loc=6.108*exp((17.27*tmin_loc)/(tmin_loc+273.3))
